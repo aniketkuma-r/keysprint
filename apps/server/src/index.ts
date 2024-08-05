d2af6dc31b99d89ea3ee4fed6f3857a0e858a214
Aniket Kumar <aniketkumar8743130@gmail.com>
2024-08-05 15:56:44 +0530

    update server directory structure

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -3,7 +3,11 @@ import { Server } from "socket.io";
 import { setupListeners } from "./setupListeners";
 
 const PORT = process.env.PORT || 8000;
-const httpServer = createServer();
+const httpServer = createServer((req, res) => {
+  if (req.method === 'GET' && req.url === '/') {
+    res.end('Server is running, keySprint is a multiplayer online browser-based typing game. allows people to race each-other by typing.');
+  }
+});
 
 const io = new Server(httpServer, {
   cors: {
